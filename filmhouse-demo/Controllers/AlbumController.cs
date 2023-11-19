2ed6bdac9ca4e9c09f39d729c6959762271727c2
tonyzhangshi81 <tonyzhangshi@163.com>
2023-11-19 16:58:17 +0800

    mock修改

--- filmhouse-demo/Controllers/AlbumController.cs
+++ filmhouse-demo/Controllers/AlbumController.cs
@@ -6,7 +6,7 @@ namespace demo.Controllers
 {
     public class AlbumController : Controller
     {
-        public IActionResult Index(int offset = 0)
+        public IActionResult Index()
         {
             ViewBag.Title = "专辑";
             ViewBag.PageType = 2;
